38bf8d67583f24e1e62540dd1fde8e218a232e77
Vibianus <j6fm06503@gmail.com>
2016-08-10 00:19:40 +0800

    新增畫九宮格function

---
+++ drawboard.py
@@ -0,0 +1,28 @@
+def drawboard(position):
+    if position == 1:
+        print('X口口')
+    elif position == 2:
+        print('口X口')
+    elif position == 3:
+        print('口口X')
+    else:
+        print('口口口')
+
+    if position == 4:
+        print('X口口')
+    elif position == 5:
+        print('口X口')
+    elif position == 6:
+        print('口口X')
+    else:
+        print('口口口')
+        
+    if position == 7:
+        print('X口口')
+    elif position == 8:
+        print('口X口')
+    elif position == 9:
+        print('口口X')
+    else:
+        print('口口口')
+    
